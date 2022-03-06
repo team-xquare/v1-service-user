1608ee897292051a8770528aa191ffad2a80bb3c
jhhong0509 <jhhong0509@naver.com>
2022-03-07 08:55:55 +0900

    feat: project setting
    from spring initializer, any correct setting done
    
    Fixes: #3

---
+++ src/main/kotlin/com/xquare/v1userservice/V1UserServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.xquare.v1userservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class V1UserServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<V1UserServiceApplication>(*args)
+}
