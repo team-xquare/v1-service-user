2229df7b5d856bcdef9a01fcd78120eaf7d2724b
jhhong0509 <jhhong0509@naver.com>
2022-04-14 08:19:59 +0900

    feat: update repository

--- user-domain/src/main/kotlin/com/xquare/v1userservice/user/spi/UserRepositorySpi.kt
+++ user-domain/src/main/kotlin/com/xquare/v1userservice/user/spi/UserRepositorySpi.kt
@@ -1,9 +1,11 @@
-package com.xquare.v1userservice.user.repository
+package com.xquare.v1userservice.user.spi
 
+import com.xquare.v1userservice.annotations.Repository
 import com.xquare.v1userservice.user.User
 import java.util.*
 
-interface UserRepository {
+@Repository
+interface UserRepositorySpi {
     suspend fun save(user: User): User
     suspend fun findByIdOrNull(id: UUID): User?
 }
--- user-infrastructure/src/main/kotlin/com/xquare/v1userservice/user/repository/UserRepositoryImpl.kt
+++ user-infrastructure/src/main/kotlin/com/xquare/v1userservice/user/repository/UserRepositoryImpl.kt
@@ -2,28 +2,36 @@ package com.xquare.v1userservice.user.repository
 
 import com.linecorp.kotlinjdsl.query.HibernateMutinyReactiveQueryFactory
 import com.xquare.v1userservice.user.User
+import com.xquare.v1userservice.user.UserEntity
+import com.xquare.v1userservice.user.mapper.UserDomainMapper
+import com.xquare.v1userservice.user.spi.UserRepositorySpi
 import io.smallrye.mutiny.converters.uni.UniReactorConverters
+import io.smallrye.mutiny.coroutines.awaitSuspending
 import kotlinx.coroutines.reactor.awaitSingleOrNull
 import org.springframework.stereotype.Repository
 import java.util.*
 
 @Repository
 class UserRepositoryImpl(
-    private val reactiveQueryFactory: HibernateMutinyReactiveQueryFactory
-): UserRepository {
+    private val reactiveQueryFactory: HibernateMutinyReactiveQueryFactory,
+    private val userDomainMapper: UserDomainMapper
+) : UserRepositorySpi {
     override suspend fun save(user: User): User {
+        val userEntityToSave = userDomainMapper.userDomainToEntity(user)
         reactiveQueryFactory.transactionWithFactory { session, _ ->
-            session.persist(user)
-                .await()
+            session.persist(userEntityToSave)
+                .awaitSuspending()
         }
         return user
     }
 
     override suspend fun findByIdOrNull(id: UUID): User? {
-        return reactiveQueryFactory.withFactory { session, reactiveQueryFactory ->
-            session.find(User::class.java, id)
+        val userEntity = reactiveQueryFactory.withFactory { session, reactiveQueryFactory ->
+            session.find(UserEntity::class.java, id)
                 .convert().with(UniReactorConverters.toMono())
                 .awaitSingleOrNull()
         }
+
+        return userEntity?.let { userDomainMapper.userEntityToDomain(it) }
     }
 }
