db1ed13c0f08e099ebf3e385c10375010a9f07a3
jhhong0509 <jhhong0509@naver.com>
2022-04-14 09:15:18 +0900

    feat: add security config

---
+++ user-infrastructure/src/main/kotlin/com/xquare/v1userservice/configuration/security/SecurityConfig.kt
@@ -0,0 +1,28 @@
+package com.xquare.v1userservice.configuration.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.server.SecurityWebFilterChain
+
+@Configuration
+@EnableWebFluxSecurity
+class SecurityConfig {
+
+    @Bean
+    protected fun filterChain(http: ServerHttpSecurity): SecurityWebFilterChain? {
+        return http
+            .httpBasic().disable()
+            .formLogin().disable()
+            .csrf().disable()
+            .cors().disable()
+            .authorizeExchange()
+            .anyExchange().permitAll()
+            .and().build()
+    }
+
+    @Bean
+    fun passwordEncoder() = BCryptPasswordEncoder()
+}
