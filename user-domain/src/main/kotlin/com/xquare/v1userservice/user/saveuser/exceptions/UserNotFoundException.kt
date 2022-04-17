55cde5c5bcce21bc8690318ba1ff9c275fa990b9
jhhong0509 <jhhong0509@naver.com>
2022-04-18 08:54:20 +0900

    style: fix for ktlint

--- user-domain/src/main/kotlin/com/xquare/v1userservice/user/saveuser/exceptions/UserNotFoundException.kt
+++ user-domain/src/main/kotlin/com/xquare/v1userservice/user/saveuser/exceptions/UserNotFoundException.kt
@@ -4,7 +4,7 @@ import com.xquare.v1userservice.exceptions.BaseException
 
 class UserNotFoundException(
     message: String
-): BaseException(message, 404) {
+) : BaseException(message, 404) {
     companion object {
         const val USER_ON_DELETE_NOT_FOUND = "User Not Found When Try To Update State"
     }
