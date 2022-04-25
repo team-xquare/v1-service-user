db6a2260a0c93ef76d8c5ef4a627119c57575e4e
jhhong0509 <jhhong0509@naver.com>
2022-04-25 22:28:16 +0900

    test: add Utils for user test

---
+++ user-domain/src/test/kotlin/com/xquare/v1userservice/user/UserUtils.kt
@@ -0,0 +1,21 @@
+package com.xquare.v1userservice.user
+
+import java.time.LocalDate
+import java.time.Year
+import java.util.UUID
+
+object UserUtils {
+    fun buildUserWithCreatePendingState() = User(
+        name = "name",
+        password = "testPassword",
+        accountId = "accountId",
+        birthDay = LocalDate.now(),
+        classNum = 2,
+        deviceToken = "sdaf",
+        entranceYear = Year.now().value,
+        grade = 1,
+        num = 2,
+        profileFileName = "sdaf",
+        id = UUID.randomUUID()
+    )
+}
