caec71c2b24be52adeedc8d72a172570ae02dcd7
홍정현 <57659252+jhhong0509@users.noreply.github.com>
2022-04-25 22:58:14 +0900

    feat: update UserEntity and User Domain Object (#30)

--- user-infrastructure/src/main/kotlin/com/xquare/v1userservice/user/UserEntity.kt
+++ user-infrastructure/src/main/kotlin/com/xquare/v1userservice/user/UserEntity.kt
@@ -1,12 +1,14 @@
 package com.xquare.v1userservice.user
 
 import java.time.LocalDate
-import java.time.Year
 import java.util.UUID
 import javax.persistence.Column
 import javax.persistence.Entity
+import javax.persistence.EnumType
+import javax.persistence.Enumerated
 import javax.persistence.Id
 import javax.persistence.Table
+import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 
 @Entity
@@ -16,32 +18,48 @@ class UserEntity(
     @Column(columnDefinition = "BINARY(16)")
     val id: UUID,
 
+    @field:NotNull
     @field:Size(max = 5)
     val name: String,
 
-    val entranceYear: Year,
+    @field:NotNull
+    val entranceYear: Int,
 
+    @field:NotNull
     val birthDay: LocalDate,
 
+    @field:NotNull
     val grade: Int,
 
     @field:Size(max = 255)
+    @field:NotNull
     val profileFileName: String,
 
+    @field:NotNull
     val password: String,
 
+    @field:NotNull
+    @Column(nullable = false, unique = true)
     val accountId: String,
 
     classNum: Int,
 
     num: Int,
 
-    deviceToken: String,
-) {
+    state: UserState,
 
+    deviceToken: String?,
+) {
+    @field:NotNull
+    @field:Enumerated(EnumType.STRING)
+    var state: UserState = state
+        protected set
+
+    @field:NotNull
     var classNum = classNum
         protected set
 
+    @field:NotNull
     var num = num
         protected set
 
