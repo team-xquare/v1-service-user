59e861a51e01bb486822fde160718c9b4591d2e0
jhhong0509 <jhhong0509@naver.com>
2022-04-25 22:35:56 +0900

    test: add stub of UserRepository

---
+++ user-domain/src/main/kotlin/com/xquare/v1userservice/stubs/InMemoryUserRepository.kt
@@ -0,0 +1,33 @@
+package com.xquare.v1userservice.stubs
+
+import com.xquare.v1userservice.annotations.Stub
+import com.xquare.v1userservice.user.User
+import com.xquare.v1userservice.user.UserState
+import com.xquare.v1userservice.user.saveuser.spi.UserRepositorySpi
+import java.util.UUID
+
+@Stub
+class InMemoryUserRepository(
+    private val userMap: HashMap<UUID, User> = hashMapOf()
+) : UserRepositorySpi {
+
+    override suspend fun saveUserAndOutbox(user: User): User {
+        userMap[user.id] = user
+        return user
+    }
+
+    override suspend fun findByIdAndStateOrNull(id: UUID, state: UserState): User? {
+        val user = userMap[id]
+        return if (user?.state == state) user else null
+    }
+
+    override suspend fun applyChanges(user: User): User {
+        userMap[user.id] = user
+        return user
+    }
+
+    override suspend fun deleteByIdAndState(id: UUID, userState: UserState): User? {
+        val userToDelete = userMap[id]
+        return if (userToDelete?.state == userState) userMap.remove(id) else null
+    }
+}
