611004427577775f138ca7e92de43d1849b57e99
jhhong0509 <jhhong0509@naver.com>
2022-04-29 08:38:26 +0900

    feat: UserRouter

--- user-infrastructure/src/main/kotlin/com/xquare/v1userservice/user/router/UserEventRouter.kt
+++ user-infrastructure/src/main/kotlin/com/xquare/v1userservice/user/router/UserEventRouter.kt
@@ -1,9 +1,11 @@
 package com.xquare.v1userservice.user.router
 
-import com.xquare.v1userservice.user.event.usercreate.AuthorityCreatedEvent
+import com.xquare.v1userservice.configuration.cdc.DebeziumMessage
 import com.xquare.v1userservice.user.event.usercreate.AuthorityCreatedFailedEvent
 import com.xquare.v1userservice.user.handler.UserEventHandler
 import com.xquare.v1userservice.user.handler.UserExceptionEventHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.support.KafkaHeaders
 import org.springframework.messaging.handler.annotation.Header
@@ -12,26 +14,27 @@ import org.springframework.stereotype.Controller
 @Controller
 class UserEventRouter(
     private val userEventHandler: UserEventHandler,
-    private val userExceptionEventHandler: UserExceptionEventHandler
+    private val userExceptionEventHandler: UserExceptionEventHandler,
+    private val coroutineScope: CoroutineScope
 ) {
 
     companion object {
-        const val USER_RESPONSE_TOPIC = "authority-create-response"
-        const val USER_ERROR_TOPIC_PREFIX = "user-response"
+        const val USER_RESPONSE_TOPIC = "xquare-debezium2.authentication.tbl_outbox"
+        const val USER_ERROR_TOPIC_PREFIX = "user-error-"
     }
 
     @KafkaListener(topics = [USER_RESPONSE_TOPIC], groupId = "user-service")
-    suspend fun authorityCreatedEventHandler(
-        authorityCreatedEvent: AuthorityCreatedEvent
-    ) {
-        userEventHandler.authorityCreatedEventHandler(authorityCreatedEvent)
+    fun authorityCreatedEventHandler(
+        debeziumMessage: DebeziumMessage
+    ) = coroutineScope.launch {
+        userEventHandler.authorityCreatedEventHandler(debeziumMessage)
     }
 
-    @KafkaListener(topicPattern = "$USER_ERROR_TOPIC_PREFIX-*", groupId = "user-service")
-    suspend fun userErrorHandler(
+    @KafkaListener(topicPattern = "${USER_ERROR_TOPIC_PREFIX}authority-creation", groupId = "user-service")
+    fun userErrorHandler(
         authorityCreatedFailedEvent: AuthorityCreatedFailedEvent,
-        @Header(KafkaHeaders.TOPIC) topic: String
-    ) {
+        @Header(KafkaHeaders.RECEIVED_TOPIC) topic: String
+    ) = coroutineScope.launch {
         userExceptionEventHandler.exceptionHandler(authorityCreatedFailedEvent, topic)
     }
 }
