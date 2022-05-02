f5087de38c05c52ec8122851e47ef398125b403a
jhhong0509 <jhhong0509@naver.com>
2022-05-02 13:57:19 +0900

    style: ktlint format

--- user-infrastructure/src/test/kotlin/com/xquare/v1userservice/domain/repository/UserRepositoryImplTest.kt
+++ user-infrastructure/src/test/kotlin/com/xquare/v1userservice/domain/repository/UserRepositoryImplTest.kt
@@ -1,42 +1,42 @@
-//package com.xquare.v1userservice.domain.repository
+// package com.xquare.v1userservice.domain.repository
 //
-//import com.linecorp.kotlinjdsl.query.HibernateMutinyReactiveQueryFactory
-//import com.linecorp.kotlinjdsl.querydsl.expression.col
-//import com.linecorp.kotlinjdsl.selectQuery
-//import com.xquare.v1userservice.EmbeddedMySQLConfiguration
-//import com.xquare.v1userservice.EqualsTestUtil
-//import com.xquare.v1userservice.configuration.cdc.OutboxEntity
-//import com.xquare.v1userservice.configuration.datasource.QueryBuilderConfig
-//import com.xquare.v1userservice.user.User
-//import com.xquare.v1userservice.user.UserState
-//import com.xquare.v1userservice.user.mapper.UserDomainMapperImpl
-//import com.xquare.v1userservice.user.repository.UserRepositoryImpl
-//import com.xquare.v1userservice.user.saveuser.spi.UserRepositorySpi
-//import java.time.LocalDate
-//import java.time.Year
-//import java.util.UUID
-//import kotlinx.coroutines.ExperimentalCoroutinesApi
-//import kotlinx.coroutines.test.runTest
-//import org.assertj.core.api.Assertions.assertThat
-//import org.junit.jupiter.api.Test
-//import org.junit.jupiter.api.assertDoesNotThrow
-//import org.springframework.boot.test.context.SpringBootTest
-//import org.springframework.test.context.TestConstructor
+// import com.linecorp.kotlinjdsl.query.HibernateMutinyReactiveQueryFactory
+// import com.linecorp.kotlinjdsl.querydsl.expression.col
+// import com.linecorp.kotlinjdsl.selectQuery
+// import com.xquare.v1userservice.EmbeddedMySQLConfiguration
+// import com.xquare.v1userservice.EqualsTestUtil
+// import com.xquare.v1userservice.configuration.cdc.OutboxEntity
+// import com.xquare.v1userservice.configuration.datasource.QueryBuilderConfig
+// import com.xquare.v1userservice.user.User
+// import com.xquare.v1userservice.user.UserState
+// import com.xquare.v1userservice.user.mapper.UserDomainMapperImpl
+// import com.xquare.v1userservice.user.repository.UserRepositoryImpl
+// import com.xquare.v1userservice.user.saveuser.spi.UserRepositorySpi
+// import java.time.LocalDate
+// import java.time.Year
+// import java.util.UUID
+// import kotlinx.coroutines.ExperimentalCoroutinesApi
+// import kotlinx.coroutines.test.runTest
+// import org.assertj.core.api.Assertions.assertThat
+// import org.junit.jupiter.api.Test
+// import org.junit.jupiter.api.assertDoesNotThrow
+// import org.springframework.boot.test.context.SpringBootTest
+// import org.springframework.test.context.TestConstructor
 //
-//@ExperimentalCoroutinesApi
-//@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
-//@SpringBootTest(
+// @ExperimentalCoroutinesApi
+// @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+// @SpringBootTest(
 //    classes = [
 //        EmbeddedMySQLConfiguration::class,
 //        QueryBuilderConfig::class,
 //        UserDomainMapperImpl::class,
 //        UserRepositoryImpl::class
 //    ]
-//)
-//internal class UserRepositoryImplTest(
+// )
+// internal class UserRepositoryImplTest(
 //    private val userRepository: UserRepositorySpi,
 //    private val reactiveQueryFactory: HibernateMutinyReactiveQueryFactory
-//) {
+// ) {
 //
 //    @Test
 //    fun saveEntity() = runTest {
@@ -84,4 +84,4 @@
 //            profileFileName = "sdaf"
 //        )
 //    }
-//}
+// }
