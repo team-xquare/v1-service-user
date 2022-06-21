15b95df0d1bfbf8ad8436f0c1e579857b378afb5
jhhong0509 <jhhong0509@naver.com>
2022-06-21 18:59:47 +0900

    feat: modify User Request

--- user-infrastructure/src/main/kotlin/com/xquare/v1userservice/user/router/dto/saveuser/CreateUserRequest.kt
+++ user-infrastructure/src/main/kotlin/com/xquare/v1userservice/user/router/dto/saveuser/CreateUserRequest.kt
@@ -1,9 +1,7 @@
 package com.xquare.v1userservice.user.router.dto.saveuser
 
-import java.time.LocalDate
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
-import javax.validation.constraints.PositiveOrZero
 import javax.validation.constraints.Size
 
 data class CreateUserRequest(
@@ -12,29 +10,10 @@ data class CreateUserRequest(
     val accountId: String,
 
     @field:NotBlank
-    @field:Size(max = 5)
-    val name: String,
+    val verificationCode: String,
 
     @field:NotBlank
-    val authCode: String,
-
-    @field:NotNull
-    val birthDay: LocalDate,
-
-    @field:PositiveOrZero
-    @field:NotNull
-    val grade: Int,
-
-    @field:PositiveOrZero
-    @field:NotNull
-    val classNum: Int,
-
-    @field:PositiveOrZero
-    @field:NotNull
-    val num: Int,
-
-    @field:NotBlank
-    val profileImageUrl: String,
+    val profileFileName: String,
 
     @field:NotNull
     val password: String
