794b86929b2da6d6bb72aaf573715a62974708c9
jhhong0509 <jhhongb0509@gmail.com>
2022-08-14 14:55:41 +0900

    fix: webClient codec을 SNAKE_CASE로 수정

--- user-infrastructure/src/main/kotlin/com/xquare/v1userservice/configuration/webclient/WebClientConfiguration.kt
+++ user-infrastructure/src/main/kotlin/com/xquare/v1userservice/configuration/webclient/WebClientConfiguration.kt
@@ -1,12 +1,23 @@
 package com.xquare.v1userservice.configuration.webclient
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.codec.ClientCodecConfigurer
+import org.springframework.http.codec.json.Jackson2JsonDecoder
+import org.springframework.web.reactive.function.client.ExchangeStrategies
 import org.springframework.web.reactive.function.client.WebClient
+
 
 @Configuration
 class WebClientConfiguration {
     @Bean
-    fun webClient(): WebClient =
-        WebClient.builder().build()
+    fun webClient(objectMapper: ObjectMapper): WebClient =
+        WebClient.builder()
+            .exchangeStrategies(ExchangeStrategies.builder()
+                .codecs { configurer: ClientCodecConfigurer ->
+                    configurer.defaultCodecs().jackson2JsonDecoder(Jackson2JsonDecoder(objectMapper))
+                }
+                .build())
+            .build()
 }
