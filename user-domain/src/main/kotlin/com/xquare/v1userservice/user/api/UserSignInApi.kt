f2e26144e54bbf46e1613b680bc17d432f118da9
jhhong0509 <jhhongb0509@gmail.com>
2022-08-15 13:29:06 +0900

    fix: Redis 조회가 안되는 오류 수정

--- user-domain/src/main/kotlin/com/xquare/v1userservice/user/api/UserSignInApi.kt
+++ user-domain/src/main/kotlin/com/xquare/v1userservice/user/api/UserSignInApi.kt
@@ -2,9 +2,8 @@ package com.xquare.v1userservice.user.api
 
 import com.xquare.v1userservice.user.api.dtos.SignInDomainRequest
 import com.xquare.v1userservice.user.api.dtos.SignInResponse
-import com.xquare.v1userservice.user.api.dtos.TokenRefreshResponse
 
 interface UserSignInApi {
     suspend fun userSignIn(signInDomainRequest: SignInDomainRequest): SignInResponse
-    suspend fun userTokenRefresh(refreshToken: String): TokenRefreshResponse
+    suspend fun userTokenRefresh(refreshToken: String): SignInResponse
 }
--- user-domain/src/main/kotlin/com/xquare/v1userservice/user/api/impl/UserSignInApiImpl.kt
+++ user-domain/src/main/kotlin/com/xquare/v1userservice/user/api/impl/UserSignInApiImpl.kt
@@ -6,7 +6,6 @@ import com.xquare.v1userservice.user.User
 import com.xquare.v1userservice.user.api.UserSignInApi
 import com.xquare.v1userservice.user.api.dtos.SignInDomainRequest
 import com.xquare.v1userservice.user.api.dtos.SignInResponse
-import com.xquare.v1userservice.user.api.dtos.TokenRefreshResponse
 import com.xquare.v1userservice.user.exceptions.PasswordNotMatchesException
 import com.xquare.v1userservice.user.exceptions.UserNotFoundException
 import com.xquare.v1userservice.user.refreshtoken.RefreshToken
@@ -59,7 +58,7 @@ class UserSignInApiImpl(
         }
     }
 
-    override suspend fun userTokenRefresh(refreshToken: String): TokenRefreshResponse {
+    override suspend fun userTokenRefresh(refreshToken: String): SignInResponse {
         val pureRefreshToken = refreshToken.split(" ").getOrNull(1)
             ?: throw InvalidRefreshTokenException("Invalid Refresh Token")
 
@@ -73,14 +72,15 @@ class UserSignInApiImpl(
 
         val params = buildAccessTokenParams(user)
 
-        saveNewRefreshToken(user, params)
+        val refreshTokenDomain = saveNewRefreshToken(user, params)
 
         val accessToken = jwtTokenGeneratorSpi.generateJwtToken(user.accountId, TokenType.ACCESS_TOKEN, params)
 
         val expireAt = LocalDateTime.now().plusHours(jwtTokenGeneratorSpi.getAccessTokenExpirationAsHour().toLong())
 
-        return TokenRefreshResponse(
+        return SignInResponse(
             accessToken = accessToken,
+            refreshToken = refreshTokenDomain.tokenValue,
             expireAt = expireAt
         )
     }
