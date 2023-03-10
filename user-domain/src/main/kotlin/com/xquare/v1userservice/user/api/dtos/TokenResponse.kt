ec6a227c319528dee73561ebdea4fe9e8220065c
정대현 <80083160+eogus2513@users.noreply.github.com>
2023-03-10 18:04:19 +0900

    feat: 유저 로그인 & 토큰 재발급 API Role 추가 (#106)

--- user-domain/src/main/kotlin/com/xquare/v1userservice/user/api/dtos/TokenResponse.kt
+++ user-domain/src/main/kotlin/com/xquare/v1userservice/user/api/dtos/TokenResponse.kt
@@ -1,9 +1,11 @@
 package com.xquare.v1userservice.user.api.dtos
 
+import com.xquare.v1userservice.user.UserRole
 import java.time.LocalDateTime
 
 data class TokenResponse(
     val accessToken: String,
     val refreshToken: String,
-    val expireAt: LocalDateTime
+    val expireAt: LocalDateTime,
+    val role: UserRole
 )
