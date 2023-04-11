6875609d4588dcc6e5d0601fe5d5fdeb9bacf509
jaemin <sonjm3518@dsm.hs.kr>
2023-04-11 23:38:36 +0900

    feat: GetUserInfoRequest

---
+++ user-infrastructure/src/main/kotlin/com/xquare/v1userservice/user/router/dto/GetUserInfoRequest.kt
@@ -0,0 +1,10 @@
+package com.xquare.v1userservice.user.router.dto
+
+import java.util.UUID
+import javax.validation.constraints.NotNull
+
+
+data class GetUserInfoRequest(
+    @field: NotNull
+    val userIds: List<UUID>,
+)
