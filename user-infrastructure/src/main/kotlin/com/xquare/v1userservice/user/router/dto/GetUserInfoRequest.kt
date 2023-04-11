218cddba2a4bc07d1905afd19fff8500d841693a
jaemin <sonjm3518@dsm.hs.kr>
2023-04-12 08:59:50 +0900

    ♻️ :: 줄바꿈 수정

--- user-infrastructure/src/main/kotlin/com/xquare/v1userservice/user/router/dto/GetUserInfoRequest.kt
+++ user-infrastructure/src/main/kotlin/com/xquare/v1userservice/user/router/dto/GetUserInfoRequest.kt
@@ -3,7 +3,6 @@ package com.xquare.v1userservice.user.router.dto
 import java.util.UUID
 import javax.validation.constraints.NotNull
 
-
 data class GetUserInfoRequest(
     @field: NotNull
     val userIds: List<UUID>,
