6201066709edccd9f76af1ac098ff517b5a218c4
Juyeong <103026521+jyk1029@users.noreply.github.com>
2023-04-28 12:59:04 +0900

    🔀 :: (#26) security 권한 설정
    * ♻️ :: security 권한 설정
    
    * 🐛 :: lint Error 해결
    
    * ⚡️ :: pattern 수정
    
    * ⚡️ :: 오타 수정

--- user-infrastructure/src/main/kotlin/com/xquare/v1userservice/configuration/security/SecurityConfig.kt
+++ user-infrastructure/src/main/kotlin/com/xquare/v1userservice/configuration/security/SecurityConfig.kt
@@ -1,7 +1,11 @@
 package com.xquare.v1userservice.configuration.security
 
+import com.xquare.v1userservice.user.UserRole
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod.PATCH
+import org.springframework.http.HttpMethod.POST
+import org.springframework.http.HttpMethod.PUT
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
@@ -11,6 +15,10 @@ import org.springframework.security.web.server.SecurityWebFilterChain
 @EnableWebFluxSecurity
 class SecurityConfig {
 
+    private val STUDENT = "ROLE_" + UserRole.STU
+    private val SCHOOL = "ROLE_" + UserRole.SCH
+    private val DOMITORY = "ROLE_" + UserRole.DOR
+
     @Bean
     protected fun filterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
         return http
@@ -19,7 +27,10 @@ class SecurityConfig {
             .csrf().disable()
             .cors().disable()
             .authorizeExchange()
-            .anyExchange().permitAll()
+            .pathMatchers(POST, "/users", "/users/login").permitAll()
+            .pathMatchers(PUT, "/users/login").permitAll()
+            .pathMatchers(PATCH, "/users").hasAnyRole(STUDENT)
+            .anyExchange().hasAnyRole(SCHOOL, DOMITORY)
             .and().build()
     }
 
