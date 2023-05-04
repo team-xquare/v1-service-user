286f06a7a795b743468c9cc8ad3a5b331ea408bf
pokabook <103029701+pokabook@users.noreply.github.com>
2023-05-04 10:02:40 +0900

    ⚡️:: @field:Pattern 추가 (#124)

--- user-infrastructure/src/main/kotlin/com/xquare/v1userservice/user/router/dto/UpdateProfileFileRequest.kt
+++ user-infrastructure/src/main/kotlin/com/xquare/v1userservice/user/router/dto/UpdateProfileFileRequest.kt
@@ -1,5 +1,8 @@
 package com.xquare.v1userservice.user.router.dto
 
+import javax.validation.constraints.Pattern
+
 data class UpdateProfileFileRequest(
+    @field:Pattern(regexp = "^.*\\.(jpg|jpeg|png|heic|webp)\$")
     val profileFileName: String?
 )
