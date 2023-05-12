80588b17abd66b0a7346120138e14f5a8dce8b0d
lyutvs <lyutvs@gmail.com>
2023-05-12 20:00:42 +0900

    ♻️ :: javadoc 추가

--- user-infrastructure/src/main/kotlin/com/xquare/v1userservice/configuration/extension/Util.kt
+++ user-infrastructure/src/main/kotlin/com/xquare/v1userservice/configuration/extension/Util.kt
@@ -1,4 +1,9 @@
 package com.xquare.v1userservice.configuration.extension
 
+/**
+ * This is a Kotlin extension function for the List class that checks if all the elements in the list are either null or blank.
+ *
+ * @return null if all the elements in the list are either null or blank. Otherwise, it returns the original list.
+ */
 fun List<String?>.nullIfBlank(): List<String?>? =
     if (this.all { it.isNullOrBlank() }) null else this
