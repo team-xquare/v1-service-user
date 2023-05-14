31469938fceeebfd5a282cbba1388c518c9e6d5f
주영 <jyk102999@dsm.hs.kr>
2023-05-14 23:16:59 +0900

    🐛 :: Trailing space  Error 해결

--- user-infrastructure/src/main/kotlin/com/xquare/v1userservice/user/router/dto/GetExcludeUserIdListRequest.kt
+++ user-infrastructure/src/main/kotlin/com/xquare/v1userservice/user/router/dto/GetExcludeUserIdListRequest.kt
@@ -3,7 +3,7 @@ package com.xquare.v1userservice.user.router.dto
 import reactor.util.annotation.Nullable
 import java.util.UUID
 
-data class GetExcludeUserIdListRequest(
+data class GetExcludeUserIdListRequest (
     
     @field:Nullable
     val userIds: List<UUID>,
