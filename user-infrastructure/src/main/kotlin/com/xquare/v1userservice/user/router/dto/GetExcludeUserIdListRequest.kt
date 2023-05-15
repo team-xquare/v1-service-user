a4364b2cbae4bb2511a7da3c2a7ecbafea95c779
김주영 <103026521+jyk1029@users.noreply.github.com>
2023-05-15 18:52:36 +0900

    🔀 :: (#81) 4096 bytes Error 해결 (#130)
    * 🐛 :: 4096 bytes Error 해결
    
    * 🐛 :: lint 해결
    
    * 🐛 :: Trailing space  Error 해결
    
    * 🐛 :: lint CI 해결

---
+++ user-infrastructure/src/main/kotlin/com/xquare/v1userservice/user/router/dto/GetExcludeUserIdListRequest.kt
@@ -0,0 +1,9 @@
+package com.xquare.v1userservice.user.router.dto
+
+import reactor.util.annotation.Nullable
+import java.util.UUID
+
+data class GetExcludeUserIdListRequest(
+    @field:Nullable
+    val userIds: List<UUID>,
+)
