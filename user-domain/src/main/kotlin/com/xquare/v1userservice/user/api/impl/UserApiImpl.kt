7c65ea98bdab889909e13ad8006365d1076793b9
주영 <jyk102999@dsm.hs.kr>
2023-07-20 08:53:12 +0900

    ♻️ :: 비밀번호 일치 리팩토링

--- user-domain/src/main/kotlin/com/xquare/v1userservice/user/api/impl/UserApiImpl.kt
+++ user-domain/src/main/kotlin/com/xquare/v1userservice/user/api/impl/UserApiImpl.kt
@@ -147,13 +147,11 @@ class UserApiImpl(
         val user = userRepositorySpi.findByAccountIdAndStateWithCreated(signInDomainRequest.accountId)
             ?: throw UserNotFoundException(UserNotFoundException.USER_ID_NOT_FOUND)
 
-        val deviceTokenModifiedUser = if (signInDomainRequest.deviceToken != "mac") {
-            userRepositorySpi.applyChanges(user.setDeviceToken(signInDomainRequest.deviceToken))
-        } else {
-            user
-        }
+        checkPasswordMatches(user, signInDomainRequest.password)
 
-        checkPasswordMatches(deviceTokenModifiedUser, signInDomainRequest.password)
+        if (signInDomainRequest.deviceToken != "mac") {
+            userRepositorySpi.applyChanges(user.setDeviceToken(signInDomainRequest.deviceToken))
+        }
 
         val params = buildAccessTokenParams(user)
 
