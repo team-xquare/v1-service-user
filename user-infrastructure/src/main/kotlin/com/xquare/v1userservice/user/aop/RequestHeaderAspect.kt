5d1298dbdb1bf1d70fb05a086f2eb7408420c698
김주영 <103026521+jyk1029@users.noreply.github.com>
2023-08-22 08:18:51 +0900

    🔀 :: secret value 삭제 (#144)
    * ♻️ :: secret value 삭제
    
    * ♻️ :: 안 쓰는 import 삭제

--- user-infrastructure/src/main/kotlin/com/xquare/v1userservice/user/aop/RequestHeaderAspect.kt
+++ user-infrastructure/src/main/kotlin/com/xquare/v1userservice/user/aop/RequestHeaderAspect.kt
@@ -1,7 +1,6 @@
 package com.xquare.v1userservice.user.aop
 
 import com.xquare.v1userservice.jwt.UnAuthorizedException
-import com.xquare.v1userservice.user.exceptions.InvalidSecretValueException
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
@@ -18,12 +17,5 @@ class RequestHeaderAspect(
             ?: throw UnAuthorizedException("UnAuthorized")
 
         return UUID.fromString(userId)
-    }
-
-    fun getSecretValue(serverRequest: ServerRequest) {
-        val secretValue = serverRequest.headers().firstHeader("Request-Xquare-Secret")
-        if ((secretValue == null) || (secretValue != secret)) {
-            throw InvalidSecretValueException("Secret is invalid")
-        }
     }
 }
