576a5e6a0a67928a669d03d33ea7033e999dcc48
주영 <jyk102999@dsm.hs.kr>
2023-08-21 16:05:28 +0900

    ♻️ :: secret value 주석 처리

--- user-infrastructure/src/main/kotlin/com/xquare/v1userservice/user/aop/RequestHeaderAspect.kt
+++ user-infrastructure/src/main/kotlin/com/xquare/v1userservice/user/aop/RequestHeaderAspect.kt
@@ -1,16 +1,16 @@
 package com.xquare.v1userservice.user.aop
 
 import com.xquare.v1userservice.jwt.UnAuthorizedException
-import com.xquare.v1userservice.user.exceptions.InvalidSecretValueException
-import org.springframework.beans.factory.annotation.Value
+/*import com.xquare.v1userservice.user.exceptions.InvalidSecretValueException
+import org.springframework.beans.factory.annotation.Value*/
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
 import java.util.UUID
 
 @Component
 class RequestHeaderAspect(
-    @Value("\${security.value}")
-    private val secret: String,
+    /*@Value("\${security.value}")
+    private val secret: String,*/
 ) {
 
     fun getUserId(serverRequest: ServerRequest): UUID {
@@ -20,10 +20,10 @@ class RequestHeaderAspect(
         return UUID.fromString(userId)
     }
 
-    fun getSecretValue(serverRequest: ServerRequest) {
+    /*fun getSecretValue(serverRequest: ServerRequest) {
         val secretValue = serverRequest.headers().firstHeader("Request-Xquare-Secret")
         if ((secretValue == null) || (secretValue != secret)) {
             throw InvalidSecretValueException("Secret is invalid")
         }
-    }
+    }*/
 }
