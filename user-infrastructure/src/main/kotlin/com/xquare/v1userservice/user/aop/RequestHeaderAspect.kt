4f1a53d180efb358b88d230f2fcf1947939a951f
주영 <jyk102999@dsm.hs.kr>
2023-08-21 14:36:05 +0900

    ♻️ :: 안 쓰는 import 삭제

--- user-infrastructure/src/main/kotlin/com/xquare/v1userservice/user/aop/RequestHeaderAspect.kt
+++ user-infrastructure/src/main/kotlin/com/xquare/v1userservice/user/aop/RequestHeaderAspect.kt
@@ -1,7 +1,6 @@
 package com.xquare.v1userservice.user.aop
 
 import com.xquare.v1userservice.jwt.UnAuthorizedException
-import com.xquare.v1userservice.user.exceptions.InvalidSecretValueException
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
