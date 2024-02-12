0a7d9d6bbee9b551eb2b753829768be5b9498384
도경 <dokyungkim2926@gmail.com>
2024-02-13 02:03:27 +0900

    modify :: feign config

--- user-infrastructure/src/main/kotlin/com/xquare/v1userservice/configuration/feign/FeignConfig.kt
+++ user-infrastructure/src/main/kotlin/com/xquare/v1userservice/configuration/feign/FeignConfig.kt
@@ -1,11 +1,12 @@
 package com.xquare.v1userservice.configuration.feign
 
+import com.xquare.v1userservice.BASE_PACKAGE
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration
 import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.cloud.openfeign.FeignAutoConfiguration
 import org.springframework.context.annotation.Configuration
 
-@EnableFeignClients(basePackages = ["com.xquare.v1userservice"])
+@EnableFeignClients(basePackages = [BASE_PACKAGE])
 @ImportAutoConfiguration(FeignAutoConfiguration::class)
 @Configuration
 class FeignConfig
