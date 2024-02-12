c92bff530ceef1c02cc110f6e58130570b24cebb
soohyeon <meltapplee@gmail.com>
2024-02-13 01:52:12 +0900

    add :: yml & application annotation

--- user-infrastructure/src/main/kotlin/com/xquare/v1userservice/V1UserServiceApplication.kt
+++ user-infrastructure/src/main/kotlin/com/xquare/v1userservice/V1UserServiceApplication.kt
@@ -2,9 +2,11 @@ package com.xquare.v1userservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 
 internal const val BASE_PACKAGE = "com.xquare.v1userservice"
 
+@EnableFeignClients
 @SpringBootApplication
 class V1UserServiceApplication
 
